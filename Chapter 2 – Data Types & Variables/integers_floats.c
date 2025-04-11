9188d7888ded269eb69adf2ec1c9e2c5af1c05f9
Rajusharma1095 <rajusharma91238@gmail.com>
2025-04-11 22:45:47 +0530

    Update integers_floats.c

--- Chapter 2 – Data Types & Variables/integers_floats.c
+++ Chapter 2 – Data Types & Variables/integers_floats.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    float height = 5.9;
+    double weight = 70.55;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Weight: %.2lf\n", weight);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
