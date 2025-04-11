3dd9763df837616f7d489c94675e4989dab3e3a7
Rajusharma1095 <rajusharma91238@gmail.com>
2025-04-11 23:13:06 +0530

    Create relational_operators.c

---
+++ Chapter03_Operators/relational_operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+  int x=5, y=10;
+  printf(" x > y : %d \n", x>y):
+  printf(" x < y : %d \n", x<y):
+  printf(" x == y : %d \n", x==y):
+  printf(" x != y : %d \n", x!=y):
+}
+
+/*
+   ----Output----
+    
+    x > y : 0 
+    x < y : 1 
+    x == y : 0 
+    x != y : 1 
+   
+    */
