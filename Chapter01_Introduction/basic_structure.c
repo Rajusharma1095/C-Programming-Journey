4f986e411aea2cb0b63e2b0d974e1ae89d394b2c
Rajusharma1095 <rajusharma91238@gmail.com>
2025-04-11 22:35:11 +0530

    Create basic_structure.c

---
+++ Chapter01_Introduction/basic_structure.c
@@ -0,0 +1,10 @@
+// File : basic_structure.c
+#include <stdio.h>
+// This is main function
+int main()
+{
+  int number =10; //variable decleration
+  printf("The number is : %d \n"; number);
+  return 0;
+ 
+}
