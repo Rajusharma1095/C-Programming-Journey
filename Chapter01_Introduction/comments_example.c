3dfff54771fe3fdeaab0b2ff29544b9e6bfdc5e1
Rajusharma1095 <rajusharma91238@gmail.com>
2025-04-11 22:40:30 +0530

    Create comments_example.c

---
+++ Chapter01_Introduction/comments_example.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+  // This is single-line comment
+  printf("single-line comment shown above \n ");
+
+  /*
+   This is multi-line comment.
+   You can write across multipal line.
+   */ 
+  printf("Multi-line comment shown above \n"):
+  return 0;
+}
