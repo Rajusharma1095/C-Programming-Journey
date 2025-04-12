73d7f3fcb76ac07123124556c4b20558efbf51d9
Rajusharma1095 <rajusharma91238@gmail.com>
2025-04-12 19:36:52 +0530

    Create positive_or_not.c

---
+++ Chapter04_ControlFlow/if_else/positive_or_not.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    int num = 10;
+    if (num > 0) 
+    {
+        printf("Number is positive\n");
+    } else 
+    {
+        printf("Number is not positive\n");
+    }
+    return 0;
+}
