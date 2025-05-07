f71c1a5e1a13d180927ab8fb6d6ed2c011e963dd
Rajusharma1095 <rajusharma91238@gmail.com>
2025-05-07 16:06:23 +0530

    Update square_num.c

--- Functions in C/square_num.c
+++ Functions in C/square_num.c
@@ -1,19 +1,20 @@
-//square of any number 
+// Calculate the square of any number 
 #include <stdio.h>
 int square(int);
+
 int main()
-  {
-      int num, sq;
-      printf("Enter a sumber for doing square");
-      scanf("%d",&num);
-      sq=square(num);
-      printf("%d is square of %d",sq,num);
-      return 0;
-  }
+{
+    int num, sq;
+    printf("Enter a number for calculating its square: ");
+    scanf("%d", &num);
+    sq = square(num);
+    printf("%d is the square of %d\n", sq, num);
+    return 0;
+}
   
-  int square(int x)
-     {
-         int s;
-         s=x*x;
-         return s;
-     }
+int square(int x)
+{
+    int s;
+    s = x * x;
+    return s;
+}
